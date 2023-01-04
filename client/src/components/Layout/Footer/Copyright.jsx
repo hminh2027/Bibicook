71fa36d57aed81baa9b0c684bdb2bdaf4a70c729
pta208VDT <pta20801@gmail.com>
2023-01-04 16:04:09 +0700

    use vite

--- client/src/components/Layout/Footer/Copyright.jsx
+++
@@ -1,9 +0,0 @@
-const Copyright = () => {
-  return (
-    <div className="w-full mx-auto text-center text-gray-400 border-t border-t-gray-500 px-4 py-2">
-      Copyrights © 2022 by BVN.
-    </div>
-  );
-};
-
-export default Copyright;
