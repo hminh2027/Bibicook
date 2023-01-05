5b3bf66fa8f60624bfbe926e6c01bed103459275
pta208VDT <pta20801@gmail.com>
2023-01-05 07:36:55 +0700

    add antd

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import "antd/dist/reset.css";
+import "./index.css";
+import { LoginPage } from "./page/auth";
+const router = createBrowserRouter([
+  {
+    path: "/auth",
+    element: <LoginPage />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
